da92b56df8b897869645884b014d955209c89431
Matias Vazquez <mfvazquez@fi.uba.ar>
2020-01-14 22:22:23 -0300

    moving some dirs

--- lausane_all.sh
+++ lausane_all.sh
@@ -2,19 +2,17 @@
 function start_job {
 	echo "starting job $1" >> log.txt
 
-	export SUBJECTS_DIR=~/media/ineco/FONDECYT/FMRI_ANALYSIS_FONDECYT/00_ALL/FS/sin_lausanne
 	cd /media/ineco/FONDECYT/FMRI_ANALYSIS_FONDECYT/00_ALL/PIPELINE_FUN_ROSARIO/scripts
-	/run_lausanne.sh $1 $2
+	/run_lausanne.sh $1 /media/ineco/8TB/SOFI/FONDECYT/Preprocesamiento_todos/FREESURFER/sin_lausanne
 
 	echo "ending job $1" >> log.txt
 }
 
 
-anat_dir=/media/ineco/8TB/SOFI/FONDECYT/Preprocesamiento_todos/FREESURFER/sin_lausanne
-for file in ${anat_dir}/*.anat;
+for file in *.anat;
 do
 	filename="${file%.*}"
-	start_job $filename $anat_dir &	
+	start_job $filename &	
 done
 
 wait
